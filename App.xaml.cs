f6255dc1714b4cbabb38ddb081e756305a6c013a
Aleksander <aleksander.kolyada@gmail.com>
2024-12-24 13:39:58 +0300

    Change MainPage type to NavigationPage. Add navigation menu.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,18 @@
-﻿namespace MarkaApp
+﻿using Microsoft.Extensions.Configuration;
+
+namespace MarkaApp
 {
 	public partial class App : Application
 	{
 		public App()
 		{
 			InitializeComponent();
+			MainPage = new NavigationPage(new MainPage())
+			{
+				BarBackgroundColor = Color.FromArgb("#6834eb"),
+				BarTextColor = Colors.White
 
-			MainPage = new AppShell();
+			};
 		}
 	}
 }
