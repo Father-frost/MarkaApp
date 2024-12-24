d8d489b9ebdea49bfad9422d46a337ee30185747
Aleksander <aleksander.kolyada@gmail.com>
2024-12-24 13:50:15 +0300

    Add view model for config.

---
+++ ViewModels/ConfigViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MarkaApp.Services;
+
+namespace MarkaApp.ViewModels
+{
+	public partial class ConfigViewModel
+	{
+		private readonly ISettingsService _settingsService;
+		public ConfigViewModel(ISettingsService settingsService)
+		{
+			_settingsService = settingsService;
+		}
+
+		public async Task LoadData()
+		{
+			
+		}
+
+
+	}
+}
